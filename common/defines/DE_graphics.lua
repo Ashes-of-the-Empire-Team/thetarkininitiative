b942ec19d5e8ec23f81afb6169c4fb9c44015f31
Palpy <ajgurm@hotmail.com>
2023-11-04 01:14:08 -0400

    Lots of stuff

--- common/defines/DE_graphics.lua
+++ common/defines/DE_graphics.lua
@@ -1,7 +1,9 @@
-NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 3500
+NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 3000
 NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 800.0
 NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 850.0
 NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 900.0
-NDefines_Graphics.NGraphics.FRIEND_COLOR  = {0.8, 0.8, 1.0}
-NDefines_Graphics.NGraphics.ENEMY_COLOR   = {1.0, 0.7, 0.7}
+NDefines_Graphics.NGraphics.FRIEND_COLOR  = {1.0, 1.0, 1.0}
+NDefines_Graphics.NGraphics.ENEMY_COLOR   = {1.0, 1.0, 1.0}
 NDefines_Graphics.NGraphics.NEUTRAL_COLOR = {1.0, 1.0, 1.0}
+NDefines.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+
